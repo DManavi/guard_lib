789bed6c1c0ec787e83f7cf58d9b199443d23454
Danial Manavi <dmanavi@live.com>
2023-03-14 08:20:21 -0700

    feat: Array guards.

--- src/lib/array.ts
+++ src/lib/array.ts
@@ -1,43 +1,43 @@
 import { BadRequestError } from 'error-lib';
 
 /**
- * Check if provided argument is an array
- * @param arg Argument to check
+ * Check if provided value is an array
+ * @param val Value to check
  * @returns type check result
  */
-export const isArray = <T = any>(arg?: any): arg is Array<T> =>
-  Array.isArray(arg);
+export const isArray = <T = any>(val: any): val is Array<T> =>
+  Array.isArray(val);
 
 /**
- * Check if provided argument is a non-empty array
- * @param arg Argument to check
- * @returns Type & argument check result
+ * Check if provided value is a non-empty array
+ * @param val Value to check
+ * @returns Type & value check result
  */
-export const isNonEmptyArray = <T = any>(arg: any): arg is Array<T> =>
-  isArray(arg) === true && arg.length > 0;
+export const isNonEmptyArray = <T = any>(val: any): val is Array<T> =>
+  isArray(val) === true && val.length > 0;
 
 /**
- * Ensure provided argument is an array
- * @param arg Argument to check
- * @param argName Argument name
+ * Ensure provided value is an array
+ * @param val Value to check
+ * @param argName argument name
  */
-export const ensureIsArray = <T = any>(arg?: any, argName?: string): void => {
-  if (isArray<T>(arg) === false) {
+export const ensureIsArray = <T = any>(val: any, argName?: string): void => {
+  if (isArray<T>(val) === false) {
     throw new BadRequestError(
-      `${argName ?? 'Provided argument'} is not an array.`,
+      `${argName ?? 'Provided value'} must be an array.`,
     );
   }
 };
 
 /**
- * Ensure provided argument is a non-empty array
- * @param arg Argument to check
- * @param argName Argument name
+ * Ensure provided value is a non-empty array
+ * @param arg value to check
+ * @param argName argument name
  */
 export const ensureIsNonEmptyArray = (arg: any, argName?: string): void => {
   if (isNonEmptyArray(arg) === false) {
     throw new BadRequestError(
-      `${argName ?? 'Provided argument'} is either not an array or is empty.`,
+      `${argName ?? 'Provided value'} is either not an array or is empty.`,
     );
   }
 };
