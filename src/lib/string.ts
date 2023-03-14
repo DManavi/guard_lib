2bdcf223092734f21d6fbbcc33ee2065f4240047
Danial Manavi <dmanavi@live.com>
2023-03-14 08:57:38 -0700

    feat: string guards.

--- src/lib/string.ts
+++ src/lib/string.ts
@@ -1,27 +1,51 @@
 import { BadRequestError } from 'error-lib';
 
-export const isString = (val?: any): val is string => typeof val === 'string';
+import { ensureTypeIs } from './general';
 
-export const isEmptyString = (val?: any): val is string =>
+/**
+ * Check if the provided value is string
+ * @param val value to check
+ * @returns type check result
+ */
+export const isString = (val: any): val is string => typeof val === 'string';
+
+/**
+ * Check if the provided value is an empty string
+ * @param val Value to check
+ * @returns type check result
+ */
+export const isEmptyString = (val: any): val is string =>
   isString(val) === true && val.length === 0;
 
+/**
+ * Check if the provided value is a non-empty string
+ * @param val Value to check
+ * @returns type check result
+ */
 export const isNonEmptyString = (val?: any): val is string =>
   isEmptyString(val) === false;
 
-export const ensureIsString = (val?: any, argName?: string): void => {
-  if (isString(val) === false) {
-    throw new BadRequestError(
-      `${argName ?? 'Provided argument'} is not a string.`,
-    );
-  }
-};
+/**
+ * Ensure the provided value is a string
+ * @param val Value to check
+ * @param argName Argument name
+ */
+export const ensureIsString = (val: any, argName?: string): void =>
+  ensureTypeIs(val, 'string', argName);
 
-export const ensureIsNonEmptyString = (val?: any, argName?: string): void => {
+/**
+ * Ensure the provided value is a non-empty string
+ * @param val Value to check
+ * @param argName Argument name
+ */
+export const ensureIsNonEmptyString = (val: any, argName?: string): void => {
+  // type check
+  ensureIsString(val, argName);
+
+  // emptiness check
   if (isNonEmptyString(val) === false) {
     throw new BadRequestError(
-      `${
-        argName ?? 'Provided argument'
-      } is either not a string or is an empty string.`,
+      `${argName ?? 'Provided value'} is an empty string.`,
     );
   }
 };
